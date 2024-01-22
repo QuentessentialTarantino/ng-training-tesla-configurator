5a4b0046bf8309e152b255b03028174ddca1f9b7
Johnny Gérard <john@jgerard.dev>
2024-01-22 23:00:23 +0100

    feat: add nav bar, routing and step components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { StepOneComponent } from './step-one/step-one.component';
+import { StepTwoComponent } from './step-two/step-two.component';
+import { StepThreeComponent } from './step-three/step-three.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/step-one', pathMatch: 'full' },
+  { path: 'step-one', component: StepOneComponent },
+  { path: 'step-two', component: StepTwoComponent },
+  { path: 'step-three', component: StepThreeComponent },
+  { path: '**', redirectTo: '/step-one' }
+];
