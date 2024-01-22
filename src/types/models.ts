a87a5b11b6d2131e3b0fe3b20f0a85b22b71cd51
Johnny Gérard <john@jgerard.dev>
2024-01-23 00:03:27 +0100

    refactor: move Color type to separate file

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,13 +1,9 @@
+import { Color } from "./color";
+
 export type Models = Model[];
 
 type Model = {
   code: string;
   description: string;
   colors: Color[];
-};
-
-type Color = {
-  code: string;
-  description: string;
-  price: number;
 };
