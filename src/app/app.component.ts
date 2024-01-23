0f61086af711db71f02ae8474286c2eb8948bc96
Johnny Gérard <john@jgerard.dev>
2024-01-23 01:54:17 +0100

    feat: add model viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ModelViewerComponent } from './model-viewer/model-viewer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    ModelViewerComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
