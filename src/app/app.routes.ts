6c14832fb14db721bd3f9f010782ec94cf7ddd08
Johnny Gérard <john@jgerard.dev>
2024-01-24 05:00:48 +0100

    feat: add router guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,22 @@ import { Routes } from '@angular/router';
 import { StepOneComponent } from './step-one/step-one.component';
 import { StepTwoComponent } from './step-two/step-two.component';
 import { StepThreeComponent } from './step-three/step-three.component';
+import { inject } from '@angular/core';
+import { ModelService } from './model.service';
+import { OptionService } from './option.service';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/step-one', pathMatch: 'full' },
   { path: 'step-one', component: StepOneComponent },
-  { path: 'step-two', component: StepTwoComponent },
-  { path: 'step-three', component: StepThreeComponent },
+  {
+    path: 'step-two',
+    component: StepTwoComponent,
+    canActivate: [() => inject(ModelService).modelCode$.value.length > 0]
+  },
+  {
+    path: 'step-three',
+    component: StepThreeComponent,
+    canActivate: [() => inject(OptionService).configId > 0]
+  },
   { path: '**', redirectTo: '/step-one' }
 ];
